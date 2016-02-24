6e8f5cd30da40ef68447be3b37d98a8d78014114
Unicorn IT <unicornit@unicorns-mini.fritz.box>
2016-02-24 12:36:07 +0100

    Initial Commit

---
+++ MyFramework/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Unicorn IT on 24.02.16.
+//  Copyright © 2016 com.my.framework. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
